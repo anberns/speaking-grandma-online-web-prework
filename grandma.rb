1a285794f77cae9994335c4b66ae4841ac9d5fbd
Aaron Berns <anberns@gmail.com>
2019-01-03 20:34:03 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(words)
   if words == "I LOVE YOU GRANDMA!"
-    "I LOVE YOU TOO PUMPKIN"
+    "I LOVE YOU TOO PUMPKIN!"
   elsif words == words.upcase
     "NO, NOT SINCE 1938!"
   else
